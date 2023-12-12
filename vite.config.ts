6be7f9730f9bc036d7f6e7affe127d98873e23b2
zhongzhaoli <525966315@qq.com>
2023-12-12 19:43:06 +0800

    chore: vite脚手架基本配置，环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,33 @@
-import { defineConfig } from 'vite';
+import { type UserConfigExport, type ConfigEnv, loadEnv } from 'vite';
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()]
-});
+export default (configEnv: ConfigEnv): UserConfigExport => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd());
+  const { VITE_PUBLIC_PATH } = viteEnv;
+  return {
+    base: VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    server: {
+      // host设置为true，可根据ip的变化，用ip进行访问
+      host: true,
+      // 设置端口号
+      port: 5173,
+      // 允许跨域访问
+      cors: true,
+      // 不自动打开浏览器
+      open: false,
+      // 端口被占用时，不直接退出
+      strictPort: false,
+      // 预热常用文件
+      warmup: {
+        clientFiles: ['./src/{views,components}/**/*.{vue,ts}']
+      }
+    },
+    plugins: [vue()]
+  };
+};
