97de9a175449862410754b5f81d702d4bf8168c6
zhongzhaoli <525966315@qq.com>
2023-12-13 15:33:34 +0800

    chore: Pinia与持久化引入

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { loadPlugins } from '@/plugins';
+import store from '@/store';
 
 const app = createApp(App);
 // 加载插件
 loadPlugins(app);
+// Pinia
+app.use(store);
 
 app.mount('#app');
