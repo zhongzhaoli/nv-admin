e5e5587de717146c9a4be806e168e37fb928d723
zhongzhaoli <525966315@qq.com>
2024-01-24 07:42:33 +0800

    feat: element I18n变更

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="appStore.language === 'zh' ? zhCn : en">
+    <router-view />
+  </el-config-provider>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from '@/store/modules/app';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import en from 'element-plus/es/locale/lang/en';
+const appStore = useAppStore();
+</script>
 <style scoped lang="scss"></style>
